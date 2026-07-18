17e258b0551cb804861f370959e7bdec43ac0ebf
PabloViniegra <pablovpmadrid@gmail.com>
2026-07-18 22:56:12 +0200

    feat: enhance body background with repeating linear gradients for improved visual depth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { JobsDashboard } from "./_components/jobs-dashboard";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center bg-background font-sans">
+    <div className="flex flex-1 flex-col items-center font-sans">
       <JobsDashboard />
     </div>
   );
